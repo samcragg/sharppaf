f2fba25eb69c4ab9e53dd47ff85e0d3075719d86
Samuel Cragg <samcragg@users.noreply.github.com>
2014-09-09 18:54:06 +0000

    Added PostcodeFormatter for formatting/validating postcode. Removed case changing methods in AddressFormatter as PAF data is not stored in uppercase, as was implied by the specification.

---
+++ SharpPaf/PostcodeStrictValidator.cs
@@ -0,0 +1,230 @@
+﻿namespace SharpPaf
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Performs more rigorous validation of a postcode.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1126:PrefixCallsCorrectly",
+            Justification = "This is the only derived class of PostcodeValidator")]
+    internal sealed class PostcodeStrictValidator : PostcodeValidator
+    {
+        /// <summary>
+        /// Initializes a new instance of the PostcodeStrictValidator class.
+        /// </summary>
+        /// <param name="options">The options to use while validating.</param>
+        public PostcodeStrictValidator(PostcodeOptions options)
+            : base(options)
+        {
+        }
+
+        /// <inheritdoc />
+        public override int CheckInwardCode(string postcode, int index)
+        {
+            var iterator = new ReverseIterator(this.SkipInvalid, postcode, index);
+
+            if (IsValidInwardAlpha(iterator.GetNext()) &&
+                IsValidInwardAlpha(iterator.GetNext()) &&
+                IsNumber(iterator.GetNext()))
+            {
+                return iterator.Index;
+            }
+
+            return InvalidPostcode;
+        }
+
+        /// <inheritdoc />
+        public override int CheckOutwardCode(string postcode, int index)
+        {
+            // Three basic scenarios:
+            // + Letter Number Letter (Letter) - AA1A/A1A
+            // + Number Number Letter (Letter) - AA11/A11
+            // + Number Letter (Letter) - AA1/A1
+            var iterator = new ReverseIterator(this.SkipInvalid, postcode, index);
+            var stack = new CharStack(3);
+            iterator = CheckOutwardCodeEnd(iterator, ref stack);
+
+            char current = iterator.GetNext();
+            if (IsLetter(current))
+            {
+                stack.Push(current);
+
+                int start;
+                ReverseIterator lookAhead = iterator;
+                current = lookAhead.GetNext();
+                if (IsLetter(current))
+                {
+                    stack.Push(current);
+                    start = lookAhead.Index;
+                }
+                else
+                {
+                    start = iterator.Index;
+                }
+
+                if (AreValidOutwardLetters(stack))
+                {
+                    return start;
+                }
+            }
+
+            return InvalidPostcode;
+        }
+
+        private static bool AreValidOutwardLetters(CharStack stack)
+        {
+            // All post codes start with a letter so there will always be at
+            // least one letter in the stack for this method to get called, so
+            // no need to check if the stack is empty here.
+            if (!IsValidFirstAlpha(stack.Pop()))
+            {
+                return false;
+            }
+
+            if (!stack.Empty && !IsValidSecondAlpha(stack.Pop()))
+            {
+                return false;
+            }
+
+            return stack.Empty || IsValidThirdAlpha(stack.Pop());
+        }
+
+        private static ReverseIterator CheckOutwardCodeEnd(ReverseIterator iterator, ref CharStack stack)
+        {
+            // This checks for outward parts ending in 1A, 11 or 1
+            char endChar = iterator.GetNext();
+            if (IsLetter(endChar))
+            {
+                if (!IsNumber(iterator.GetNext()))
+                {
+                    return ReverseIterator.Invalid;
+                }
+
+                stack.Push(endChar);
+            }
+            else if (IsNumber(endChar))
+            {
+                ReverseIterator lookAhead = iterator;
+                if (IsNumber(lookAhead.GetNext()))
+                {
+                    return lookAhead;
+                }
+            }
+            else
+            {
+                return ReverseIterator.Invalid;
+            }
+
+            return iterator;
+        }
+
+        private static bool IsValidFirstAlpha(char value)
+        {
+            switch (StringUtils.ToUpper(value))
+            {
+                case 'Q':
+                case 'V':
+                case 'X':
+                    return false;
+
+                default:
+                    return true;
+            }
+        }
+
+        private static bool IsValidInwardAlpha(char value)
+        {
+            // We Unlike the other IsValid helpers in this class, we're not sure
+            // we've even been passed a letter in this method so we need to
+            // check that first before the switch on the disallowed letters.
+            value = StringUtils.ToUpper(value);
+            if ((value < 'A') | (value > 'Z'))
+            {
+                return false;
+            }
+
+            switch (value)
+            {
+                case 'C':
+                case 'I':
+                case 'K':
+                case 'M':
+                case 'V':
+                    return false;
+
+                default:
+                    return true;
+            }
+        }
+
+        private static bool IsValidSecondAlpha(char value)
+        {
+            switch (StringUtils.ToUpper(value))
+            {
+                case 'I':
+                case 'Z':
+                    return false;
+
+                default:
+                    return true;
+            }
+        }
+
+        private static bool IsValidThirdAlpha(char value)
+        {
+            switch (StringUtils.ToUpper(value))
+            {
+                case 'A':
+                case 'B':
+                case 'C':
+                case 'D':
+                case 'E':
+                case 'F':
+                case 'G':
+                case 'H':
+                case 'J':
+                case 'K':
+                case 'P':
+                case 'S':
+                case 'T':
+                case 'U':
+                case 'V':
+                case 'W':
+                case 'X':
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        private struct CharStack
+        {
+            private readonly char[] buffer;
+            private int length;
+
+            public CharStack(int capacity)
+            {
+                this.buffer = new char[capacity];
+                this.length = 0;
+            }
+
+            public bool Empty
+            {
+                get { return this.length == 0; }
+            }
+
+            public char Pop()
+            {
+                this.length--;
+                return this.buffer[this.length];
+            }
+
+            public void Push(char value)
+            {
+                this.buffer[this.length] = value;
+                this.length++;
+            }
+        }
+    }
+}
---
+++ SharpPaf/PostcodeValidator.cs
@@ -0,0 +1,173 @@
+﻿namespace SharpPaf
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Performs basic validation on a postcode.
+    /// </summary>
+    internal class PostcodeValidator
+    {
+        /// <summary>Protected because PostcodeStrictValidator needs access.</summary>
+        protected const int InvalidPostcode = -1;
+
+        /// <summary>Protected because PostcodeStrictValidator needs access.</summary>
+        protected readonly bool SkipInvalid;
+
+        /// <summary>
+        /// Initializes a new instance of the PostcodeValidator class.
+        /// </summary>
+        /// <param name="options">The options to use while validating.</param>
+        public PostcodeValidator(PostcodeOptions options)
+        {
+            this.SkipInvalid = options.HasFlag(PostcodeOptions.SkipInvalidCharacters);
+        }
+
+        /// <summary>
+        /// Gets the index of the start of the inward code.
+        /// </summary>
+        /// <param name="postcode">The value to check.</param>
+        /// <param name="index">The index to the end of the postcode.</param>
+        /// <returns>
+        /// The start of the inward part of the postcode, or -1 the part is invalid.
+        /// </returns>
+        public virtual int CheckInwardCode(string postcode, int index)
+        {
+            var iterator = new ReverseIterator(this.SkipInvalid, postcode, index);
+
+            if (IsLetter(iterator.GetNext()) &&
+                IsLetter(iterator.GetNext()) &&
+                IsNumber(iterator.GetNext()))
+            {
+                return iterator.Index;
+            }
+
+            return InvalidPostcode;
+        }
+
+        /// <summary>
+        /// Gets the index of the start of the outward code.
+        /// </summary>
+        /// <param name="postcode">The value to check.</param>
+        /// <param name="index">
+        /// The index to the start of the inward part or of the whitespace
+        /// between the parts.
+        /// </param>
+        /// <returns>
+        /// The start of the outward part of the postcode, or -1 the part is invalid.
+        /// </returns>
+        public virtual int CheckOutwardCode(string postcode, int index)
+        {
+            // Three basic scenarios:
+            // + Letter Number Letter (Letter) - AA1A/A1A
+            // + Number Number Letter (Letter) - AA11/A11
+            // + Number Letter (Letter) - AA1/A1
+            var iterator = new ReverseIterator(this.SkipInvalid, postcode, index);
+            iterator = CheckOutwardCodeEnd(iterator);
+            if (IsLetter(iterator.GetNext()))
+            {
+                ReverseIterator lookAhead = iterator;
+                return IsLetter(lookAhead.GetNext()) ? lookAhead.Index : iterator.Index;
+            }
+
+            return InvalidPostcode;
+        }
+
+        /// <summary>
+        /// Determines whether the specified value is a valid postcode character.
+        /// </summary>
+        /// <param name="value">The character to test.</param>
+        /// <returns>
+        /// true if the character could appear in a postcode; otherwise, false.
+        /// </returns>
+        internal static bool IsValidCharacter(char value)
+        {
+            return IsNumber(value) || IsLetter(value);
+        }
+
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented",
+            Justification = "This method is only visible to PostcodeStrictValidator")]
+        protected static bool IsLetter(char value)
+        {
+            return ((value >= 'A') & (value <= 'Z')) ||
+                   ((value >= 'a') & (value <= 'z'));
+        }
+
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented",
+            Justification = "This method is only visible to PostcodeStrictValidator")]
+        protected static bool IsNumber(char value)
+        {
+            return (value >= '0') & (value <= '9');
+        }
+
+        private static ReverseIterator CheckOutwardCodeEnd(ReverseIterator iterator)
+        {
+            // This checks for outward parts ending in 1A, 11 or 1
+            char endChar = iterator.GetNext();
+            if (IsLetter(endChar))
+            {
+                if (!IsNumber(iterator.GetNext()))
+                {
+                    return ReverseIterator.Invalid;
+                }
+            }
+            else if (IsNumber(endChar))
+            {
+                ReverseIterator lookAhead = iterator;
+                if (IsNumber(lookAhead.GetNext()))
+                {
+                    return lookAhead;
+                }
+            }
+            else
+            {
+                return ReverseIterator.Invalid;
+            }
+
+            return iterator;
+        }
+
+        // Note: This must be a struct for easy copying when looking ahead for
+        // a value.
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented",
+            Justification = "This structure is only visible to PostcodeStrictValidator")]
+        protected struct ReverseIterator
+        {
+            public static readonly ReverseIterator Invalid = new ReverseIterator(false, null, -1);
+
+            private readonly string postcode;
+            private readonly bool skipInvalid;
+            private int index;
+
+            public ReverseIterator(bool skipInvalid, string postcode, int index)
+            {
+                this.postcode = postcode;
+                this.skipInvalid = skipInvalid;
+                this.index = index + 1; // We always subtract one from the index when calling GetNext
+            }
+
+            public int Index
+            {
+                get { return this.index; }
+            }
+
+            public char GetNext()
+            {
+                while (this.index > 0)
+                {
+                    this.index--;
+                    char c = this.postcode[this.index];
+                    if ((this.skipInvalid && !IsValidCharacter(c)) ||
+                        StringUtils.IsWhitespace(c))
+                    {
+                        continue;
+                    }
+
+                    return c;
+                }
+
+                this.index = -1;
+                return '\0';
+            }
+        }
+    }
+}
