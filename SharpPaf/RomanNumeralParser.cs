b2ee87bb5225f0c5c3a56f24147f05de9d30621c
samcragg_cp <SND\samcragg_cp>
2014-09-28 06:18:26 +0000

    Added TitleCaseConverter, which produces output to match the CSV PAF files.

---
+++ SharpPaf/RomanNumeralParser.cs
@@ -0,0 +1,144 @@
+﻿namespace SharpPaf
+{
+    /// <summary>
+    /// Allows matching of Roman numerals up to 99.
+    /// </summary>
+    internal sealed class RomanNumeralParser
+    {
+        private readonly IMatcher matcher;
+
+        /// <summary>
+        /// Initializes a new instance of the RomanNumeralParser class.
+        /// </summary>
+        public RomanNumeralParser()
+        {
+            // This is the regex we're matching (from http://stackoverflow.com/a/267405):
+            //     (XC|XL|L?X{0,3})(IX|IV|V?I{0,3})
+            //
+            // Changing the optionals to a range and grouping things gives:
+            //     (XC | XL | (L{0,1} X{0,3})) (IX | IV | (V{0,1} I{0,3})))
+            IMatcher tens = new MatchAny(new MatchTwoChars("XC"), new MatchTwoChars("XL"), new MatchBoth(new MatchZeroOrMore('L', 1), new MatchZeroOrMore('X', 3)));
+            IMatcher ones = new MatchAny(new MatchTwoChars("IX"), new MatchTwoChars("IV"), new MatchBoth(new MatchZeroOrMore('V', 1), new MatchZeroOrMore('I', 3)));
+            this.matcher = new MatchBoth(tens, ones);
+        }
+
+        private interface IMatcher
+        {
+            int Match(string input, int index);
+        }
+
+        /// <summary>
+        /// Determines whether the specified string contains a Roman numeral in
+        /// the specified range.
+        /// </summary>
+        /// <param name="value">The string to check.</param>
+        /// <param name="start">The index of the first character to check.</param>
+        /// <param name="end">The index of the last character to check.</param>
+        /// <returns>
+        /// true if the specified range is a valid Roman numeral; otherwise, false.
+        /// </returns>
+        /// <remarks>
+        /// The string must be uppercase for it to match.
+        /// </remarks>
+        public bool IsMatch(string value, int start, int end)
+        {
+            return this.matcher.Match(value, start) == end;
+        }
+
+        private class MatchAny : IMatcher
+        {
+            private readonly IMatcher[] matchers;
+
+            public MatchAny(params IMatcher[] matchers)
+            {
+                this.matchers = matchers;
+            }
+
+            public int Match(string input, int index)
+            {
+                for (int i = 0; i < this.matchers.Length; i++)
+                {
+                    int newIndex = this.matchers[i].Match(input, index);
+                    if (newIndex != index)
+                    {
+                        return newIndex;
+                    }
+                }
+
+                return index;
+            }
+        }
+
+        private class MatchBoth : IMatcher
+        {
+            private readonly IMatcher first;
+            private readonly IMatcher second;
+
+            public MatchBoth(IMatcher first, IMatcher second)
+            {
+                this.first = first;
+                this.second = second;
+            }
+
+            public int Match(string input, int index)
+            {
+                index = this.first.Match(input, index);
+                return this.second.Match(input, index);
+            }
+        }
+
+        private class MatchTwoChars : IMatcher
+        {
+            private readonly char first;
+            private readonly char second;
+
+            public MatchTwoChars(string value)
+            {
+                this.first = value[0];
+                this.second = value[1];
+            }
+
+            public int Match(string input, int index)
+            {
+                if (index > input.Length - 2)
+                {
+                    return index;
+                }
+
+                if ((input[index] != this.first) | (input[index + 1] != this.second))
+                {
+                    return index;
+                }
+
+                return index + 2;
+            }
+        }
+
+        private class MatchZeroOrMore : IMatcher
+        {
+            private readonly char match;
+            private readonly int max;
+
+            public MatchZeroOrMore(char value, int maximum)
+            {
+                this.match = value;
+                this.max = maximum;
+            }
+
+            public int Match(string input, int index)
+            {
+                for (int i = 0; i < this.max; i++)
+                {
+                    if ((index >= input.Length) || (input[index] != this.match))
+                    {
+                        break;
+                    }
+
+                    index++;
+                }
+
+                return index;
+            }
+        }
+    }
+}
---
+++ SharpPaf/TitleCaseConverter.cs
@@ -0,0 +1,212 @@
+﻿namespace SharpPaf
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Allows the conversion from uppercase to title case, where each word
+    /// starts with a capital letter.
+    /// </summary>
+    /// <remarks>
+    /// This class has been tweaked to match the output of the CSV address
+    /// format, e.g. <c>MCDONALD</c> would become <c>McDonald</c> and <c>UK</c>
+    /// would remain unchanged.
+    /// </remarks>
+    public sealed class TitleCaseConverter
+    {
+        private readonly Dictionary<string, string> exceptionWords = new Dictionary<string, string>(StringComparer.Ordinal)
+        {
+            { "ACCA", "ACCA" },
+            { "AM", "AM" },
+            { "BBC", "BBC" },
+            { "BMW", "BMW" },
+            { "BP", "BP" },
+            { "CP", "CP" },
+            { "DIY", "DIY" },
+            { "GB", "GB" },
+            { "GMBH", "GmbH" },
+            { "LLP", "LLP" },
+            { "MP", "MP" },
+            { "NHS", "NHS" },
+            { "PC", "PC" },
+            { "PCT", "PCT" },
+            { "PLC", "plc" },
+            { "PM", "PM" },
+            { "RAF", "RAF" },
+            { "RC", "RC" },
+            { "TSB", "TSB" },
+            { "TV", "TV" },
+            { "UK", "UK" },
+            { "YMCA", "YMCA" }
+        };
+
+        private readonly HashSet<string> lowercaseWhenHyphenated = new HashSet<string>(StringComparer.Ordinal)
+        {
+            "AND",
+            "AT",
+            "BUT",
+            "BY",
+            "CUM",
+            "FOR",
+            "IN",
+            "LE",
+            "OF",
+            "ON",
+            "OR",
+            "PLC",
+            "SO",
+            "THE",
+            "TO",
+            "UP",
+            "Y",
+            "YET",
+        };
+
+        private readonly RomanNumeralParser romanNumeral = new RomanNumeralParser();
+
+        /// <summary>
+        /// Converts the specified uppercased value into title case.
+        /// </summary>
+        /// <param name="value"
+        /// >The value to convert, which is all in uppercase.
+        /// </param>
+        /// <returns>
+        /// The specified value in title case, or null if <c>value</c> is null.
+        /// </returns>
+        /// <remarks>
+        /// The input string is assumed to be in uppercase; if it contains any
+        /// lowercase letters then these words may not be converted correctly
+        /// (e.g. <c>NhS</c> might incorrectly be converted to <c>Nhs</c>).
+        /// </remarks>
+        public string ToTitleCase(string value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            char[] converted = new char[value.Length];
+            bool uppercaseNext = true;
+            int start = 0;
+            for (int i = 0; i < converted.Length; i++)
+            {
+                if (uppercaseNext)
+                {
+                    converted[i] = StringUtils.ToUpper(value[i]);
+                }
+                else
+                {
+                    converted[i] = StringUtils.ToLower(value[i]);
+                }
+
+                uppercaseNext = this.NextShouldBeUppercase(value, converted, i, ref start);
+            }
+
+            this.CheckForExceptionWords(value, converted, start, value.Length);
+            EnsureStartsWithACapital(converted);
+            return new string(converted);
+        }
+
+        private static void EnsureStartsWithACapital(char[] value)
+        {
+            if (value.Length > 1)
+            {
+                if (value[0] == '(')
+                {
+                    value[1] = StringUtils.ToUpper(value[1]);
+                }
+                else
+                {
+                    value[0] = StringUtils.ToUpper(value[0]);
+                }
+            }
+        }
+
+        private static bool IsEndOfWord(string value, int index)
+        {
+            if (index > value.Length)
+            {
+                return false;
+            }
+            else if (index == value.Length)
+            {
+                return true;
+            }
+            else
+            {
+                char c = value[index];
+                return (c == ' ') | (c == '-') | (c == ')'); // Avoid branching by using bitwise-or
+            }
+        }
+
+        private void CheckForExceptionWords(string value, char[] converted, int start, int end)
+        {
+            string word = value.Substring(start, end - start);
+            string replacement;
+            if (this.exceptionWords.TryGetValue(word, out replacement))
+            {
+                for (int i = 0; i < replacement.Length; i++)
+                {
+                    converted[start + i] = replacement[i];
+                }
+            }
+            else if (this.romanNumeral.IsMatch(value, start, end))
+            {
+                for (int i = start; i < end; i++)
+                {
+                    converted[i] = StringUtils.ToUpper(converted[i]);
+                }
+            }
+        }
+
+        private void CheckForHyphenatedWords(string value, char[] converted, int start, int end)
+        {
+            // When this method is called we know that the end points to a
+            // hyphen, however, we don't know if the word also starts with one.
+            if ((start != 0) && (value[start - 1] == '-'))
+            {
+                string word = value.Substring(start, end - start);
+                if (this.lowercaseWhenHyphenated.Contains(word))
+                {
+                    converted[start] = StringUtils.ToLower(converted[start]);
+                    return;
+                }
+            }
+
+            this.CheckForExceptionWords(value, converted, start, end);
+        }
+
+        private bool NextShouldBeUppercase(string value, char[] converted, int index, ref int start)
+        {
+            switch (value[index])
+            {
+                case 'C':
+                    // Did we capitalize an M before this one (e.g. McDonald)?
+                    return (index != 0) && (converted[index - 1] == 'M');
+
+                case '-':
+                    this.CheckForHyphenatedWords(value, converted, start, index);
+                    start = index + 1; // Skip the hyphen
+                    return true;
+
+                case '(':
+                case ')':
+                case ' ':
+                    this.CheckForExceptionWords(value, converted, start, index);
+                    start = index + 1; // Skip the separator
+                    return true;
+
+                case '&':
+                case '.':
+                    // Don't start a new word just yet
+                    return true;
+
+                case '\'':
+                    return !IsEndOfWord(value, index + 2);
+
+                default:
+                    return false;
+            }
+        }
+    }
+}
